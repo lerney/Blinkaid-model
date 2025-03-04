48283d7d37135c3c48b42233b977fd2f73cd9664
lerney <lerney@post.bgu.ac.il>
2025-03-04 17:41:17 +0200

    small update

--- blinkaid_models/utils/utils.py
+++ blinkaid_models/utils/utils.py
@@ -1,7 +1,10 @@
+from scipy.signal import resample
+
 # Function to convert EMG signals into a single image
 def normalize_and_convert_to_image(signal_window,image_height,window_size,num_channels):
     """ Converts an EMG signal window into a single (image_height, window_size) image with max values. """
     normalized_signals = (signal_window - np.min(signal_window)) / (np.max(signal_window) - np.min(signal_window))
+    normalized_signals = np.clip(normalized_signals, 0, 1)
 
     image = np.zeros((image_height, window_size), dtype=np.uint8)
     value_scale = np.linspace(255, 255 - (num_channels - 1) * 10, num_channels, dtype=np.uint8)
